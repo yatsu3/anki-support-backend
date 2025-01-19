f97e35f7bdbf3ef7cbd40f7b99948cc73d7dd039
tastuya1002 <tatsu.sf1002@gmail.com>
2025-01-19 10:38:08 +0900

    問題出題APIの実装

--- src/main/java/com/example/demo/presentation/QuestionController.java
+++ src/main/java/com/example/demo/presentation/QuestionController.java
@@ -1,10 +1,13 @@
 package com.example.demo.presentation;
+import com.example.demo.domain.entity.GroupedQuestion;
 import com.example.demo.domain.service.QuestionService;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QuestionController {
@@ -28,5 +31,11 @@ public class QuestionController {
         String explanation = (String) requestBody.get("explanation");
 
         questionService.registerQuestion(categoryName, userId, question, choices, correctedAnswer, explanation);
+    }
+
+    @GetMapping(value = "/get-questions")
+    public List<GroupedQuestion> getQuestion(@RequestParam int userId, @RequestParam int categoryId) {
+        return questionService.getQuestion(userId, categoryId);
+        
     }
 }
