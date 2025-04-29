23e606f7a2d77b0e918031c1eb1f6681cc80314b
yatsu3 <yatsutako3gmail.com>
2025-04-29 10:17:07 +0900

    リリース環境疎通確認

--- src/main/java/com/example/demo/settings/SecurityConfig.java
+++ src/main/java/com/example/demo/settings/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         System.out.println("----CorsConfigurationSource----");
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // フロントエンドのオリジンを許可
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://anki-support-frontend.pages.dev")); // フロントエンドのオリジンを許可
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 許可するHTTPメソッド
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type")); // 許可するヘッダー
         configuration.setAllowCredentials(true); // 認証情報を許可
